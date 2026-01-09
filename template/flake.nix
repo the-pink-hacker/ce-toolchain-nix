fe9a32d8b83bb3860d462d6ebebcf9efb8a9d3e2
Pink Garrett <pink@thepinkhacker.com>
2026-01-09 12:58:28 -0500

    Add formatter

--- template/flake.nix
+++ template/flake.nix
@@ -1,11 +1,14 @@
 {
-  inputs.toolchain.url = "github:myclevorname/flake";
+    inputs.toolchain.url = "github:myclevorname/flake";
 
-  outputs = { self, toolchain }: {
-    packages.x86_64-linux.default = toolchain.packages.x86_64-linux.mkDerivation {
-      pname = "hi";
-      version = "0.0.1";
-      src = self;
+    outputs = {
+        self,
+        toolchain,
+    }: {
+        packages.x86_64-linux.default = toolchain.packages.x86_64-linux.mkDerivation {
+            pname = "hi";
+            version = "0.0.1";
+            src = self;
+        };
     };
-  };
 }
